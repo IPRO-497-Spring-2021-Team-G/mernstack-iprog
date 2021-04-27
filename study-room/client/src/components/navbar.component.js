72674e82e6bbead2b43ad6a4583582ecf3e78e7c
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-04-27 17:24:39 -0500

    Updated Navbar

--- study-room/client/src/components/navbar.component.js
+++ study-room/client/src/components/navbar.component.js
@@ -1,32 +1,98 @@
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import RegisterModal from './auth/RegisterModal';
+import LoginModal from './auth/LoginModal';
+import Logout from './auth/Logout';
+import { ReactReduxContext } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
+  state = {
+    isOpen: false
+  }
+  
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  }
+  
+  toggle = () => {
+    this.setState({
+      // Close if open, open if closed
+      isOpen: !this.state.isOpen
+    });
+  }
 
   render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Reserve</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-        <li className="navbar-item">
-          <Link to="/view" className="nav-link">View Tables</Link>
-          </li>          
-          
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Tables</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/history" className="nav-link">history</Link>
-          </li>
+    const { isAuthenticated, user } = this.props.auth;
 
-          <li className="navbar-item">
-          <Link to="/login" className="nav-link">log in</Link>
-          </li>
-        </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+        const authLinks = (
+            <Fragment>
+                <NavItem>
+                    <span className="navbar-text mr-3">
+                        <strong>{ user ? `Welcome, ${user.name}` : '' } </strong>
+                    </span>
+                </NavItem>
+                <NavItem>
+                    <Logout />
+                </NavItem>
+            </Fragment>
+        );
+
+        const guestLinks = (
+            <Fragment>
+              <NavItem>
+                <NavLink href="/">Reserve</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/view">View Tables</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/create">Create Tables</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/history">History</NavLink>
+              </NavItem>
+              <NavItem>
+                <RegisterModal />
+              </NavItem>
+              <NavItem>
+                <LoginModal />
+              </NavItem>
+            </Fragment>
+        );
+        
+        return (
+        <div>
+          <Navbar color="dark" dark expand="sm" className="mb-5">
+            <Container>
+              <NavbarBrand href='/'>ShoppingList</NavbarBrand>
+              <NavbarToggler onClick={this.toggle}></NavbarToggler>
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto">
+                  { isAuthenticated ? authLinks : guestLinks }
+                </Nav>
+              </Collapse>
+            </Container>
+          </Navbar>
+          </div>
+          );
+        }
+      }
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, null)(Navbar);
