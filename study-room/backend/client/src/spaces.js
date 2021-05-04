71d80e65ecb65ef511f7d4739b3b98740f78f214
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-05-04 01:32:04 -0500

    Added study spaces images, created dummy spaces file for testing.

---
+++ study-room/backend/client/src/spaces.js
@@ -0,0 +1,44 @@
+const spaces = [
+    {
+        _id: '1',
+        name: 'Study Space 1',
+        image: '/images/image1.jpg',
+        capacity: 2,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        isEmpty: 'Available'
+    },
+    {
+        _id: '2',
+        name: 'Study Space 2',
+        image: '/images/image2.jpg',
+        capacity: 2,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        isEmpty: 'Available'
+    },
+    {
+        _id: '3',
+        name: 'Study Space 3',
+        image: '/images/image3.jpg',
+        capacity: 2,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        isEmpty: 'Available'
+    },
+    {
+        _id: '4',
+        name: 'Study Space 4',
+        image: '/images/image4.jpg',
+        capacity: 2,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        isEmpty: 'Available'
+    },
+    {
+        _id: '5',
+        name: 'Study Space 5',
+        image: '/images/image5.jpg',
+        capacity: 2,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        isEmpty: 'Available'
+    }
+  ]
+  
+  export default spaces
